bf86e217a87e4b4abdabd043b97b9bd97da3a01e
aleks <aleksander.slawek1@gmail.com>
2022-08-08 20:28:42 +0200

    Add routes and views

--- app.js
+++ app.js
@@ -4,25 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/users", (req, res, next) => {
-  res.send("<div>users list</div>");
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</button></form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("hello");
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).res.send("<h1>page not found</h1>");
 });
 
 app.listen(3000);
