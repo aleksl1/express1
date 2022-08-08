49eddf7350b2bf5ce86068d969c92b403a648ff2
aleks <aleksander.slawek1@gmail.com>
2022-08-08 20:49:03 +0200

    serve html views

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).res.send("<h1>page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
 });
 
 app.listen(3000);
+
+// module.exports = path.dirname(require.main.filename);
