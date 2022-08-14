6705c8c8fab0dc09eaac1241dae430819bb9b2b3
aleks <aleksander.slawek1@gmail.com>
2022-08-14 18:53:15 +0200

    Remove pug and handlebars

--- app.js
+++ app.js
@@ -2,17 +2,8 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-// const expressHbs = require("express-handlebars");
 const app = express();
 
-// app.engine(
-//   "handlebars",
-//   expressHbs({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "handlebars",
-//   })
-// );
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -24,7 +15,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
   res.status(404).render("error", { docTitle: "error page", path: null });
 });
 
