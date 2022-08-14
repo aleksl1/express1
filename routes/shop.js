ed1743604c5077b5a3981349296bf290e1332edd
aleks <aleksander.slawek1@gmail.com>
2022-08-14 21:38:44 +0200

    Move logic to controller

--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,7 @@
-const path = require("path");
-
 const express = require("express");
-const rootDir = require("../util/path");
+const productsController = require("../controllers/products");
 const router = express.Router();
-const adminData = require("./admin");
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    docTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
