22c757031f3181d21b641d090d2e0e5aee94cc34
aleks <aleksander.slawek1@gmail.com>
2022-08-15 19:58:33 +0200

    Add orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,6 +17,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    docTitle: "Your Orders",
+    path: "/orders",
+  });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     docTitle: "Checkout",
