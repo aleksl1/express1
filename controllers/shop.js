ed90a03963460ab214a968023e34b211697be272
aleks <aleksander.slawek1@gmail.com>
2022-08-17 21:36:48 +0200

    Add postCart Add add to cart form include

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,6 +17,11 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  res.redirect("/cart");
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     docTitle: "Your Orders",
