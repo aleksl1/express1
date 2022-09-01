013a4016c69e760f9ed00ed41e14f8bd1e918edf
aleks <aleksander.slawek1@gmail.com>
2022-09-01 06:04:29 +0200

    Add db connections Add dotenv package

--- util/database.js
+++ util/database.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+const env = require("dotenv").config;
+const dbPassword = env().parsed.DB_PASSWORD;
 
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-complete",
-  password: process.env.DB_PASSWORD,
+  password: dbPassword,
 });
 
 module.exports = pool.promise();
